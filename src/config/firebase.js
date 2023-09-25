0f152c9b5ac2c10193464d5d817115dc3da0ccc1
Mirella7 <145349397+Mirella7@users.noreply.github.com>
2023-09-25 08:18:17 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,13 @@ import { initializeAuth } from 'firebase/auth';
 
 //Aqui vocês devem colocar as suas credenciais
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyC3hLHPWSBhrG0RddpEsoxO1MmdlrovayA",
+  authDomain: "mirellaifal513.firebaseapp.com",
+  projectId: "mirellaifal513",
+  storageBucket: "mirellaifal513.appspot.com",
+  messagingSenderId: "561699838245",
+  appId: "1:561699838245:web:141a6ac07c0907ae68e2cc",
+  measurementId: "G-MVTNMLVH1H"
 };
 
 const app = initializeApp(firebaseConfig);
